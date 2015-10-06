20d67a933b9a31eab3bb5d4a7a02dde5f579cfba
John Cody Nemmer <jonemmer@hotmail.com>
2015-10-06 09:35:51 -0700

    Did some clean up

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var pikePlace = {
 	minCustHour: 17,
 	maxCustHour: 88,
 	avgCookiesCust: 5.2,
+	
 	randCustHour: function() {
-		//Calculate our random num of cust/hour based on min/max
 		return Math.floor(Math.random() * (this.maxCustHour - this.minCustHour +1)) + this.minCustHour;
 	},
+	
 	totalCookieHour: function() {
 		return Math.floor(this.randCustHour() * this.avgCookiesCust);
 	}
@@ -24,15 +25,16 @@ for(var i = 0; i < hourOperation.length; i++) {
   }
 
 
-  var seaTac = {
+var seaTac = {
 	location: 'SeaTac Airport',
 	minCustHour: 6,
 	maxCustHour: 44,
 	avgCookiesCust: 1.2,
+	
 	randCustHour: function() {
-		//Calculate our random num of cust/hour based on min/max
 		return Math.floor(Math.random() * (this.maxCustHour - this.minCustHour +1)) + this.minCustHour;
 	},
+	
 	totalCookieHour: function() {
 		return Math.floor(this.randCustHour() * this.avgCookiesCust);
 	}
@@ -50,15 +52,16 @@ for(var i = 0; i < hourOperation.length; i++) {
   }
 
 
-  var southCenter = {
+var southCenter = {
 	location: 'Southcenter Mall',
 	minCustHour: 11,
 	maxCustHour: 38,
 	avgCookiesCust: 1.9,
+	
 	randCustHour: function() {
-		//Calculate our random num of cust/hour based on min/max
 		return Math.floor(Math.random() * (this.maxCustHour - this.minCustHour +1)) + this.minCustHour;
 	},
+	
 	totalCookieHour: function() {
 		return Math.floor(this.randCustHour() * this.avgCookiesCust);
 	}
@@ -81,14 +84,14 @@ var bellSQ = {
 	minCustHour: 20,
 	maxCustHour: 48,
 	avgCookiesCust: 3.3,
+	
 	randCustHour: function() {
-		//Calculate our random num of cust/hour based on min/max
 		return Math.floor(Math.random() * (this.maxCustHour - this.minCustHour +1)) + this.minCustHour;
 	},
+	
 	totalCookieHour: function() {
 		return Math.floor(this.randCustHour() * this.avgCookiesCust);
 	}
-	
 }
 
 var hourOperation = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
@@ -99,17 +102,18 @@ for(var i = 0; i < hourOperation.length; i++) {
   	var item = document.createElement("li");
   	item.appendChild(text);
   	list.appendChild(item);
-  }
+}
 
 var alki = {
 	location: 'Alki Beach',
 	minCustHour: 3,
 	maxCustHour: 24,
 	avgCookiesCust: 2.6,
+	
 	randCustHour: function() {
-		//Calculate our random num of cust/hour based on min/max
 		return Math.floor(Math.random() * (this.maxCustHour - this.minCustHour +1)) + this.minCustHour;
 	},
+	
 	totalCookieHour: function() {
 		return Math.floor(this.randCustHour() * this.avgCookiesCust);
 	}
@@ -124,7 +128,7 @@ for(var i = 0; i < hourOperation.length; i++) {
   	var item = document.createElement("li");
   	item.appendChild(text);
   	list.appendChild(item);
-  }
+}
 
 
 
@@ -150,39 +154,3 @@ for(var i = 0; i < hourOperation.length; i++) {
 
 
 
-//makeUL = function}
-
-
-// seaTac = {}
-
-
-//min
-//max
-//random
-
-//random * cookies per customer = cookies per hour
-
-//function makeUL(totalCookieHour) {
-	//for var i = 0; i< array.length; i++)
-	
-	//connect the list ro the JS
-	//var awesomeList = document.getElementById('list1');
-
-	// Create the list item;
-	//var item = document.createElement('li');
-
-	//set its contents:
-	//item.appendChild(document.createTextNode(array[i]));
-
-	// Add it to the list:
-    //    awesomeList.appendChild(item);
-    //}
-
-    // Finally, return the constructed list:
-    //return awesomeList;
-//}
-
-//var stuff = ["this", "that", "another thing"];
-//makeUL(stuff);
-
-//}
